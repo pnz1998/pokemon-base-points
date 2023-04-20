9186ba860b69b2479c05d424f932a75caa4dd94e
nanzhe21 <parknc@aliyun.com>
2023-04-20 17:22:25 +0800

    display card

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomePageComponent implements OnInit {
 
   constructor() { }
+  public queryCondition: string = '';
 
   ngOnInit(): void {
   }
